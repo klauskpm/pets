da0a45326414638b24761a8ffb2cf86b6c718cfb
Klaus Kazlauskas <klausmachado@gmail.com>
2016-11-09 22:01:42 -0200

    Fixing a typo

--- app/src/main/java/com/example/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -20,7 +20,7 @@ public class PetDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String SQL_CREATE_TABLE_PETS = "CREATE TABBLE " + PetEntry.TABLE_NAME + " ("
+        String SQL_CREATE_TABLE_PETS = "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
                 + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
                 + PetEntry.COLUMN_PET_BREED + " TEXT, "
