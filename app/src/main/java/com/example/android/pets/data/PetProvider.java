bf4ef9c3d2407c65de1aa8ef1e0c09e179df5e5c
Klaus Kazlauskas <klausmachado@gmail.com>
2016-11-16 02:56:39 -0200

    Content provider for Pets

---
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -0,0 +1,51 @@
+package com.example.android.pets.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Kazlauskas on 16/11/2016.
+ */
+
+public class PetProvider extends ContentProvider {
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    private PetDbHelper mPetDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        mPetDbHelper = new PetDbHelper(getContext());
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
