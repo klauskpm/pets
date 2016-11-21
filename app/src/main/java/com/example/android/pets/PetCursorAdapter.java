c38a0bb06287ce8ba2fcfcfa02dd33e51fec6e5f
Klaus Kazlauskas <klausmachado@gmail.com>
2016-11-21 10:07:48 -0200

    ViewHolder pattern with CursorAdapter

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -15,27 +15,35 @@ import com.example.android.pets.data.PetContract;
  */
 
 public class PetCursorAdapter extends CursorAdapter {
+
     public PetCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+        ViewHolder holder = new ViewHolder();
+
+        holder.name = (TextView) view.findViewById(R.id.name);
+        holder.summary = (TextView) view.findViewById(R.id.summary);
+
+        view.setTag(holder);
+
+        return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        TextView tvName = (TextView) view.findViewById(R.id.name);
-        TextView tvSummary = (TextView) view.findViewById(R.id.summary);
+        ViewHolder holder = (ViewHolder) view.getTag();
 
         String name = cursor.getString(cursor
                 .getColumnIndexOrThrow(PetContract.PetEntry.COLUMN_PET_NAME));
         String summary = cursor.getString(cursor
                 .getColumnIndexOrThrow(PetContract.PetEntry.COLUMN_PET_BREED));
 
-        tvName.setText(name);
-        tvSummary.setText(summary);
+        holder.name.setText(name);
+        holder.summary.setText(summary);
     }
 
     private class ViewHolder {
